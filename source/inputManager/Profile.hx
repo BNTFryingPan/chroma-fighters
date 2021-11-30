c7afa9c84aa8cba8af0f9157c37bff8a0f877898
LeOtOmAs <marycolageo@gmail.com>
2021-11-30 07:14:22 -0500

    stuff lmao

--- source/inputManager/Profile.hx
+++ source/inputManager/Profile.hx
@@ -3,30 +3,194 @@ package inputManager;
 import flixel.FlxG;
 import flixel.input.gamepad.FlxGamepad;
 import flixel.input.keyboard.FlxKey;
+import flixel.util.typeLimit.OneOfFour;
+import flixel.util.typeLimit.OneOfThree;
 import flixel.util.typeLimit.OneOfTwo;
 import inputManager.GenericInput.INPUT_STATE;
 import inputManager.GenericInput.InputHelper;
 import inputManager.InputManager.Action;
 import inputManager.controllers.GenericController;
+import lime.system.System;
 
-typedef ProfileAction = OneOfTwo<FlxKey, GenericButton>;
+typedef ProfileInputSource = OneOfThree<FlxKey, GenericButton, GenericAxis>;
+typedef ProfileActionSource = OneOfFour<FlxKey, GenericButton, GenericAxis, ProfileInput>;
 
-class Profile {
-    public static function getProfile(name:String) {
-        // TODO : load and create the players profile
-        return new Profile();
+enum ProfileInputType {
+    AXIS;
+    BUTTON;
+}
+
+typedef ProfileInputOptions = {
+    public var type:ProfileInputType; // the type of output this input is (axis or button)
+    public var source:ProfileInputSource; // the actual button or axis of the input
+    // if source is axis
+    public var ?deadzone:Float; // if axis value is less than this value, it becomes 0
+    // button options
+    public var ?minThreshold:Float; // if the source type is an axis, the lowest axis value to trigger this button
+    public var ?maxThreshold:Float; // ^ but the max value. could be used for different actions at different values
+    // axis options
+    public var ?digitalThreshold:Float; // the minimum value of the axis to count as a digital input (not sure when itll be used though)
+    public var ?value:Float; // the axis value if the source type is a button
+}
+
+class ProfileInput
+
+class ProfileInput {
+    public static function getFromProfileAction(action:ProfileAction):ProfileInput {
+        if (Std.isOfType(action, ProfileInput))
+            return cast action;
+
+        if (Std.isOfType(action, GenericAxis)) {
+            return new ProfileInput({
+                type: AXIS,
+                source: cast action,
+                digitalThreshold: 0.25,
+                deadzone: 0.05,
+                minThreshold: 0.05,
+                maxThreshold: 1
+            });
+        }
+        // must be FlxKey or GenericButton
+        return new ProfileInput({
+            type: BUTTON,
+            source: cast action,
+            value: 1,
+        });
     }
 
+    private var source:ProfileInputSource;
+    private var rawOptions:ProfileInputOptions;
+
+    public final type:ProfileInputType;
+
+    public var deadzone:Null<Float> = 0.05;
+    public var minThreshold:Null<Float> = 0.05;
+    public var maxThreshold:Null<Float> = 1;
+    public var digitalThreshold:Null<Float> = 0.25;
+    public var value:Null<Float> = 1;
+
+    public function new(options:ProfileInputOptions) {
+        this.rawOptions = options;
+        this.type = options.type;
+        this.source = options.source;
+
+        if (this.type == BUTTON) {
+            if (Std.isOfType(this.source, GenericAxis)) {
+                this.minThreshold = options.minThreshold;
+                this.maxThreshold = options.maxThreshold;
+            } else {}
+        }
+    }
+
+    public function getDigitalState(?gamepad:GenericController):Bool {
+        if (Std.isOfType(this.source, FlxKey)) {} else {
+            if (Std.isOfType(gamepad, Null)) {
+                return false
+            } else if (Std.isOfType(this.source, GenericButton))
+                return InputHelper.isPressed(gamepad.getButtonState(this.source))
+            else
+                return
+        }
+    }
+
+    public function getInputState(?gamepad:GenericController):INPUT_STATE {
+        if (this.type == AXIS) {
+            return NOT_PRESSED; // you cant really "press" an output axis. axis input can be "pressed" though
+        }
+        if (Std.isOfType(this.source, FlxKey)) {
+
+        }
+    }
+}
+
+class Profile {
+    public static function getProfile(name:String, useDefaultControls:Bool = false):Profile {
+        // TODO : load and create the players profile
+        var profile = new Profile();
+        if (useDefaultControls) {
+            profile.fileName = "@default";
+        } else {
+            profile.fileName = #if mobile System.applicationStorageDirectory #else System.userDirectory + "\\chroma_fighters" #end + "\\profiles";
+        }
+        profile.loadBindings();
+        return profile;
+    }
+
+    public static var defaultProfile = Profile.getProfile("", true);
+    public static var profiles:Array<String>;
+
+    // public static function getFileNameOfProfileName()
     public var name:String;
     public var fileName:String;
 
-    public var bindings:Map<Action, Array<ProfileAction>> = [
-        MENU_CONFIRM => [FlxKey.Z, FACE_A], MENU_CANCEL => [X, FACE_B], MENU_ACTION => [C, FACE_X, FACE_Y], MENU_LEFT => [A, LEFT_TRIGGER, LEFT_BUMPER],
-        MENU_RIGHT => [S, RIGHT_TRIGGER, RIGHT_BUMPER], JUMP => [X, FACE_X, FACE_Y], SHORT_JUMP => [V, RIGHT_BUMPER], ATTACK => [FlxKey.Z, FACE_A],
-        SPECIAL => [C, FACE_B], STRONG => [D], TAUNT => [F, DPAD_UP, DPAD_DOWN, DPAD_LEFT, DPAD_RIGHT],
-        SHIELD => [S, LEFT_BUMPER, LEFT_TRIGGER, RIGHT_TRIGGER], WALK => [A]];
+    public static var defaultBindings:Map<Action, Array<ProfileAction>> = [
+        MENU_CONFIRM => [
+            ProfileInput.getFromProfileAction(FlxKey.Z),
+            ProfileInput.getFromProfileAction(FACE_A)
+        ],
+        MENU_CANCEL => [
+            ProfileInput.getFromProfileAction(FlxKey.X),
+            ProfileInput.getFromProfileAction(FACE_B)
+        ],
+        MENU_ACTION => [
+            ProfileInput.getFromProfileAction(FlxKey.C),
+            ProfileInput.getFromProfileAction(FACE_X),
+            ProfileInput.getFromProfileAction(FACE_Y)
+        ],
+        MENU_LEFT => [
+            ProfileInput.getFromProfileAction(FlxKey.A),
+            ProfileInput.getFromProfileAction(LEFT_TRIGGER),
+            ProfileInput.getFromProfileAction(LEFT_BUMPER)
+        ],
+        MENU_RIGHT => [
+            ProfileInput.getFromProfileAction(FlxKey.S),
+            ProfileInput.getFromProfileAction(RIGHT_TRIGGER),
+            ProfileInput.getFromProfileAction(RIGHT_BUMPER)
+        ],
+        JUMP => [
+            ProfileInput.getFromProfileAction(FlxKey.X),
+            ProfileInput.getFromProfileAction(FACE_X),
+            ProfileInput.getFromProfileAction(FACE_Y)
+        ],
+        SHORT_JUMP => [
+            ProfileInput.getFromProfileAction(FlxKey.V),
+            ProfileInput.getFromProfileAction(RIGHT_BUMPER)
+        ],
+        ATTACK => [
+            ProfileInput.getFromProfileAction(FlxKey.Z),
+            ProfileInput.getFromProfileAction(FACE_A)
+        ],
+        SPECIAL => [
+            ProfileInput.getFromProfileAction(FlxKey.C),
+            ProfileInput.getFromProfileAction(FACE_B)
+        ],
+        STRONG => [ProfileInput.getFromProfileAction(FlxKey.D)],
+        TAUNT => [
+            ProfileInput.getFromProfileAction(FlxKey.F),
+            ProfileInput.getFromProfileAction(DPAD_UP),
+            ProfileInput.getFromProfileAction(DPAD_DOWN),
+            ProfileInput.getFromProfileAction(DPAD_LEFT),
+            ProfileInput.getFromProfileAction(DPAD_RIGHT)
+        ],
+        SHIELD => [
+            ProfileInput.getFromProfileAction(FlxKey.S),
+            ProfileInput.getFromProfileAction(LEFT_BUMPER),
+            ProfileInput.getFromProfileAction(LEFT_TRIGGER),
+            ProfileInput.getFromProfileAction(RIGHT_TRIGGER)
+        ],
+        WALK => [ProfileInput.getFromProfileAction(FlxKey.A)]
+    ];
+
+    public var bindings:Map<Action, Array<ProfileAction>> = Profile.defaultBindings;
 
     public function new() {}
+
+    public function loadBindings() {
+        if (this.fileName == "@default") {
+            // do nothing. just keep default bindings
+            return;
+        }
+    }
 
     private var input:Null<GenericInput>;
 
