890f072ed9ef4f571f325c5c8c50f6637203c947
Frying☆Pan <7706960+LeotomasMC@users.noreply.github.com>
2022-03-30 09:34:55 -0400

    loop finished?

--- source/scripting/ScriptCompiler.hx
+++ source/scripting/ScriptCompiler.hx
@@ -57,31 +57,80 @@ class ScriptCompiler {
                expr(value);
                switch (node) {
                   case NIdentifier(p, name): {
-                        add(ASet(p, node.getParameters()[1]));
-                     }
+                     add(ASet(p, node.getParameters()[1]));
+                  }
                   default: throw 'Expression is not settable at $p';
                }
             }
-         case NWhile(p, condition, expr):
-            {}
-         case NWhileDo(p, condition, expr):
-            {}
+         case NWhile(p, condition, expression):
+            {
+               // label1: condition; jump_unless label2
+               var cont_pos = actions.length;
+               expr(condition);
+               add(AJumpUnless(p, 0));
+               // <loop> jump label1
+               var start_pos = actions.length;
+               expr(expression);
+               add(AJump(p, cont_pos));
+               // label2: break
+               var break_pos = actions.length;
+               actions[start_pos - 1] = AJumpUnless(p, break_pos);
+               patch(start_pos, break_pos, break_pos, cont_pos);
+            }
+         case NWhileDo(p, condition, expression):
+            {
+               // label1: loop
+               var start_pos = actions.length;
+               expr(expression);
+               // label2: condition; jumpif label1
+               var cont_pos = actions.length;
+               expr(condition);
+               add(AJumpIf(p, start_pos));
+               // label3: break
+               var break_pos = actions.length;
+               patch(start_pos, break_pos, break_pos, cont_pos);
+            }
          case NFor(p, init, condition, post, node):
-            {}
+            {
+               expr(init);
+               // label1: condition jumpunless label3
+               var loop_pos = actions.length;
+               expr(condition);
+               add(AJumpUnless(p, 0));
+               // loop
+               var start_pos = actions.length;
+               expr(node);
+               // label2: cont, post, jump label1
+               var cont_pos = actions.length;
+               expr(post);
+               add(AJump(p, loop_pos));
+               // label3: break
+               var break_pos = actions.length;
+               actions[start_pos - 1] = AJumpUnless(p, break_pos);
+               patch(start_pos, break_pos, break_pos, cont_pos);
+            }
          case NBreak(p):
-            {}
+            add(AJump(p, -10));
          case NContinue(p):
-            {}
+            add(AJump(p, -11));
       }
    }
 
    private static function patch(start, end, _break, _continue) {
       var i = start;
       while (i < end) {
-         var act = actions[i++];
-         if (act.match(AJump(_, _))) {
-            // switch (act)
+         switch (actions[i]) {
+            case AJump(p, to):
+               {
+                  if (to == -10) {
+                     actions[i] = AJump(p, _break);
+                  } else if (to == -11) {
+                     actions[i] = AJump(p, _continue);
+                  }
+               }
+               default:
          }
+         i++;
       }
    }
 
