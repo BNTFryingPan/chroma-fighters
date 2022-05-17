c7829fd78bfa047bc6522270f7ccaac40801f150
LeotomasMC <7706960+LeotomasMC@users.noreply.github.com>
2022-05-17 12:22:31 -0400

    slowdown after match end

--- source/states/sub/MatchResults.hx
+++ source/states/sub/MatchResults.hx
@@ -1,5 +1,6 @@
 package states.sub;
 
+import flixel.FlxG;
 import flixel.FlxSubState;
 import flixel.text.FlxText;
 import states.BaseState.ChromaFightersState;
@@ -7,6 +8,9 @@ import states.BaseState.ChromaFightersState;
 class MatchResults extends FlxSubState implements ChromaFightersState {
    var text:FlxText;
    var name:String;
+
+   var waittime:Float = 0;
+   final maxtime:Float = 5;
 
    public function stateId():String {
       return 'Match Results <- ${this.parentStateId()}';
@@ -30,5 +34,22 @@ class MatchResults extends FlxSubState implements ChromaFightersState {
       this.text.scrollFactor.set(0, 0);
       add(this.text);
       this.text.screenCenter(XY);
+   }
+
+   override public function update(elapsed:Float) {
+      super.update(elapsed);
+      if (FlxG.timeScale > 0.1) {
+         FlxG.timeScale -= elapsed;
+      } else {
+         if (waittime > maxtime)
+            FlxG.switchState(new CharSelectScreen());
+         else
+            waittime += (elapsed / FlxG.timeScale);
+      }
+   }
+
+   override public function destroy() {
+      super.destroy();
+      FlxG.timeScale = 1.0;
    }
 }
